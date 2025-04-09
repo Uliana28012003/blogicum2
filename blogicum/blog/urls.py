2a1a889d2902855a512a0960442f5b066bf3c62a
Petrenko Uliana <petrenkoulya@yandex.ru>
2025-04-09 14:33:19 +0300

    Исправили admin

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
-from .views import index, post_detail, category_posts
-
-#Маршруты для ленты постов, деталей, категорий
-app_name = 'blog'
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from blog import views
+from django.contrib import admin
 
 urlpatterns = [
-    path('', index, name='index'),  # Главная страница блога
-    path('posts/<int:id>/', post_detail, name='post_detail'),  # Страница отдельного поста
-    path('category/<str:category_slug>/', category_posts, name='category_posts'),  # Страница категории
-]
-
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),  # Подключаем URLs для блога
+    path('pages/', include('pages.urls')),  # Подключаем URLs для страниц
+    path('', views.index, name='index'),  # Главная страница
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
